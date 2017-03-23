516b6cddafddfcc9ea7e069a404b9ce9b27b0eaf
Alejandro Espinosa <alex.espinosadelosmonteros@gmail.com>
2017-03-22 22:49:23 -0600

    Added action creators

---
+++ client/actions/action-creators.js
@@ -0,0 +1,27 @@
+// increment likes
+// payload
+export function increment(index) {
+    return {
+        type: 'INCREMENT_LIKES',
+        index
+    }
+}
+
+// add comment
+export function addComment(postId, author, comment) {
+    return {
+        type: 'ADD_COMMENT',
+        postId,
+        author,
+        comment
+    }
+}
+
+// remove comment
+export function removeComment(postId, index) {
+    return {
+        type: 'DELETE_COMMENT',
+        index,
+        postId
+    }
+}
