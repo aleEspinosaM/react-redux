dd8b63174984d0000789069078c1d158b3e51418
Alejandro Espinosa <alex.espinosadelosmonteros@gmail.com>
2017-03-23 23:07:08 -0600

    showing photos in Photo

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Photo from './Photo';
 
 export default class PhotoGrid extends React.Component {
     constructor() {
@@ -8,7 +9,7 @@ export default class PhotoGrid extends React.Component {
     render() {
         return (
             <div className="photo-grid">
-                im photogrid
+                {this.props.posts.map((post, index) => <Photo {...this.props} key={index} index={index} post={post}/>)}
             </div>
         )
     }
