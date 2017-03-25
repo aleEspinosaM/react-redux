542f27b09b49cff80871949c9cb3af2e036f452b
Alejandro Espinosa <alex.espinosadelosmonteros@gmail.com>
2017-03-25 11:31:52 -0600

    incrementing likes reducer

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -4,8 +4,20 @@
 
  
 function posts(state = [], action) {
-    console.log(state, action);
-    return state;
+    // do no mutate state use a pure function
+    switch(action.type) {
+        case 'INCREMENT_LIKES' :  
+        // return updated state
+        return [
+            ...state.slice(0,action.index), //before the one we are updating
+            {...state[action.index], likes: state[action.index].likes + 1},
+            ...state.slice(action.index + 1)
+        ]
+
+        // the reason we return a default value is because all reducers run for everysingle one
+        default :
+            return state;
+    }
 }
 
 
