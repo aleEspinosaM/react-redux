129656f1d44b7ece745eed1bf326475a41aa94c9
Alejandro Espinosa <alex.espinosadelosmonteros@gmail.com>
2017-03-25 19:26:56 -0600

    Added comment component

---
+++ client/components/Comments.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class componentName extends Component {
+    render() {
+        return (
+            <div className="comment">
+                im a commet
+            </div>
+        );
+    }
+}
+
+export default componentName;
+
+
+
+
+
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments'
 
 export default class Single extends React.Component {
     constructor() {
@@ -6,9 +8,14 @@ export default class Single extends React.Component {
     }
 
     render() {
+        // index of the post
+        const index = this.props.posts.findIndex((post) => post.code == this.props.params.postId);
+        const post = this.props.posts[index];
+        // console.log(post)
         return (
             <div className="single-photo">
-                im SINGLE
+                <Photo index={index} post={post} {...this.props} />
+                <Comments />
             </div>
         )
     }
