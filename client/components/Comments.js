3b03bcc340c7264709d7af777fa8754e1c453f37
Alejandro Espinosa <alex.espinosadelosmonteros@gmail.com>
2017-03-25 19:53:52 -0600

    Showing comments and comment form

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 
-class componentName extends Component {
+class Comments extends Component {
+
+    renderComment(comment, index) {
+        return (
+            <div className="comment" key={index}>  
+                <p>
+                    <strong>{comment.user}</strong>
+                    {comment.text}
+                    <button className="remove-comment">&times;</button>
+                </p>
+            </div>
+        )
+    }
     render() {
         return (
-            <div className="comment">
-                im a commet
+            <div className="comments">
+                {this.props.postComments.map(this.renderComment)}
+                <form ref="commentForm" className="comment-form">
+                    <input type="text" ref="author" placeholder="author"/>
+                    <input type="text" ref="comment" placeholder="comment"/>
+                    <input type="submit" hidden/>
+                </form>
             </div>
         );
     }
 }
 
-export default componentName;
+export default Comments;
 
 
 
--- client/components/Single.js
+++ client/components/Single.js
@@ -8,14 +8,16 @@ export default class Single extends React.Component {
     }
 
     render() {
+        const {postId} = this.props.params
         // index of the post
-        const index = this.props.posts.findIndex((post) => post.code == this.props.params.postId);
+        const index = this.props.posts.findIndex((post) => post.code == postId);
         const post = this.props.posts[index];
+        const postComments = this.props.comments[postId] || []
         // console.log(post)
         return (
             <div className="single-photo">
                 <Photo index={index} post={post} {...this.props} />
-                <Comments />
+                <Comments postComments={postComments}/>
             </div>
         )
     }
